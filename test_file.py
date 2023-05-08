ed17c3e67bf59306091150fff9a613744014f97d
RemagDedrocsid <justin2003.jn@gmail.com>
2023-05-08 03:10:46 -0500

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,6 +1,20 @@
 import functions
+import pytest
 
-def test_get_data():
+@pytest.fixture
+def mock_yf_ticker(mocker):
+    mock_ticker = mocker.Mock()
+    mock_ticker.info = {
+        'longName': 'Apple Inc.',
+        'regularMarketPrice': 138.95,
+        'regularMarketChange': 0.49,
+        'regularMarketChangePercent': 0.35,
+        'marketCap': 234400000000,
+        'regularMarketVolume': 7900000
+    }
+    mocker.patch('functions.yf.Ticker', return_value=mock_ticker)
+
+def test_get_data(mock_yf_ticker):
     data = functions.get_data('AAPL')
     assert isinstance(data, dict)
     assert 'symbol' in data
