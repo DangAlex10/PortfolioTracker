b5468922635bd797b1d94e2bcd4deb8c9e500dcb
RemagDedrocsid <justin2003.jn@gmail.com>
2023-05-08 03:38:41 -0500

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,6 +1,8 @@
 import pytest
-from unittest.mock import MagicMock
+import json
+from unittest.mock import MagicMock, mock
 import yfinance as yf
+import plotly.graph_objs as go
 from functions import get_data, get_chart
 
 
@@ -29,8 +31,33 @@ def test_get_data():
     assert data['market_cap'] == '2.20 trillion'
     assert data['volume'] == '10.00 million'
 
-def test_get_chart():
-    symbol = 'AAPL'
+@pytest.fixture
+def mock_yf_ticker_chart():
+    # Create a mock object for yf.Ticker class
+    mock_ticker = mock.Mock()
+    mock_ticker.info = {'longName': 'Mock Stock'}
+    mock_ticker_data = mock.Mock()
+    mock_ticker_data.history.return_value = {
+        'Open': [100, 110, 120],
+        'High': [120, 130, 140],
+        'Low': [90, 100, 110],
+        'Close': [110, 120, 130],
+        'Date': ['2022-05-01', '2022-05-02', '2022-05-03']
+    }
+    mock_ticker.return_value = mock_ticker_data
+    return mock_ticker
+
+
+def test_get_chart(mock_yf_ticker_chart):
+    # Test the get_chart function with the mock object
+    symbol = 'mock'
     chart = get_chart(symbol)
-    assert isinstance(chart, str)
-    assert len(chart) > 0
+    chart_data = json.loads(chart)
+    candlestick_data = chart_data['data'][0]['ohlc']
+    line_data = chart_data['data'][1]['y']
+    
+    assert chart_data['layout']['title']['text'] == 'Mock Stock Stock Chart'
+    assert len(candlestick_data['open']) == 3
+    assert candlestick_data['open'][0] == 100
+    assert candlestick_data['high'][1] == 130
+    assert line_data[2] == 130
