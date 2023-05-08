5f972b5e83d850921f563f3994f33aa20cba7f47
RemagDedrocsid <justin2003.jn@gmail.com>
2023-05-08 03:47:28 -0500

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,10 +1,5 @@
 import pytest
-import json
-from unittest import mock
-from unittest.mock import patch
-import pandas as pd
-import yfinance as yf
-import plotly.graph_objs as go
+from unittest.mock import Mock
 from functions import get_data, get_chart
 
 
@@ -32,27 +27,3 @@ def test_get_data():
     assert data['percent_change'] == '1.81'
     assert data['market_cap'] == '2.20 trillion'
     assert data['volume'] == '10.00 million'
-
-@patch('functions.yf.Ticker')
-def test_get_chart(mock_yf_ticker):
-    # Set up the mock object
-    mock_stock_data = {
-        'Open': [100, 150, 200],
-        'High': [125, 175, 225],
-        'Low': [75, 125, 175],
-        'Close': [120, 170, 220],
-        'Volume': [1000000, 2000000, 3000000],
-    }
-    mock_ticker = mock_yf_ticker.return_value
-    mock_ticker.history.return_value = pd.DataFrame(mock_stock_data)
-
-    # Test the get_chart function with the mock object
-    symbol = 'AAPL'
-    chart = get_chart(symbol)
-
-    # Check that the mock object was called correctly
-    mock_yf_ticker.assert_called_once_with(symbol)
-    mock_ticker.history.assert_called_once_with(period='1y')
-
-    # Check that the chart is not None
-    assert chart is not None
