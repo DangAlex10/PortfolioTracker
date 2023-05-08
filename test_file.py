5646fdfd4ee5ce24e3241aea99d97060ee141580
RemagDedrocsid <justin2003.jn@gmail.com>
2023-05-08 03:08:53 -0500

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,25 +1,12 @@
-# content of test_functions.py
+import functions
 
-from functions import get_data
-
-def test_get_data_valid_symbol():
-    symbol = 'AAPL' # Apple Inc. stock symbol
-    data = get_data(symbol)
-    assert data['symbol'] == symbol
-    assert data['name'] == 'Apple Inc.'
-    assert data['price'][0] == '$'
-    assert data['change'] is not None
-    assert data['percent_change'][-1] == '%'
-    assert data['market_cap'] is not None
-    assert data['volume'] is not None
-
-def test_get_data_invalid_symbol():
-    symbol = 'INVALID'
-    data = get_data(symbol)
-    assert data['symbol'] == symbol
-    assert data['name'] is None
-    assert data['price'] is None
-    assert data['change'] is None
-    assert data['percent_change'] is None
-    assert data['market_cap'] is None
-    assert data['volume'] is None
+def test_get_data():
+    data = functions.get_data('AAPL')
+    assert isinstance(data, dict)
+    assert 'symbol' in data
+    assert 'name' in data
+    assert 'price' in data
+    assert 'change' in data
+    assert 'percent_change' in data
+    assert 'market_cap' in data
+    assert 'volume' in data
