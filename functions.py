e30c2574bd16e3954c78e7147e499c3325171ca7
RemagDedrocsid <justin2003.jn@gmail.com>
2023-05-08 02:57:31 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -9,11 +9,5 @@ def get_data(symbol):
     stock = yf.Ticker(symbol)
     data = {
         'symbol': symbol,
-        'name': stock.info['longName'],
-        'price': str(stock.info['regularMarketPrice']),
-        'change': stock.info['regularMarketChange'],
-        'percent_change': str(round(stock.info['regularMarketChangePercent'], 2)),
-        'market_cap': humanize.intword(stock.info['marketCap'], format='%.2f'),
-        'volume': humanize.intword(stock.info['regularMarketVolume'], format='%.2f')
     }
     return data
